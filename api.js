d55fd8b55bb2bf7c67de1983debc35b58a505a14
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-03-25 16:36:59 -0400

    final update

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
+
 export async function getposts() {
-try {
+    try {
 const res = await fetch(BASE_URL);
 if (!res.ok){
     throw new Error(`Error: ${res.status}`);
--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 import { getposts } from './api.js';
 
 async function displayposts() {
-    const container = document.getElementById('postcontainer');
+    const container = document.getElementById('postContainer');
 
-    try{
+    try {
       const posts = await getposts();
       posts.slice(0, 5).forEach(post => {
         const div = document.createElement('div');
         div.innerHTML = `<h3> ${post.title}</h3><p>${post.body}</p>`;
+        container.appendChild(div);
 });
     } catch (err){
-        container.innerHTML = `<p style = "color:red;"> Failed to load posts: ${(err.message)}</p>`
+        container.innerHTML = `<p style = "color:red;"> Failed to load posts: ${err.message} </p>`
     }
 }
 displayposts();
